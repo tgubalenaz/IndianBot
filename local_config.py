fe5eabb3dca0683f1f03419bea87533a5b23796e
rxrx3 <70162537+rxrx3@users.noreply.github.com>
2020-08-27 22:49:20 +0200

    Create local_config.py

---
+++ local_config.py
@@ -0,0 +1,7 @@
+from heroku_config import Var
+
+class Development(Var):
+  APP_ID = 1516656
+  # 6 is the length of api id
+  API_HASH = "d9e0d8d4268862852c7ca848de66ccc7"
+  # Use Your Own Api Hash
