abea3ce090f2a3e32245fe8f0633717872b7f382
rxrx3 <70162537+rxrx3@users.noreply.github.com>
2020-08-27 23:16:03 +0200

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,7 +6,7 @@ import os
 from telethon import TelegramClient
 from var import Var
 from userbot.utils import load_module
-from userbot import LOAD_PLUG, LOGS
+from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
